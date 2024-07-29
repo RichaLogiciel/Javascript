6f93060ca4b19a083ef3ad31b018968e7819f74d
RichaLogiciel <richa@logiciel.io>
2024-07-29 12:44:48 +0530

    container changes done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "aemail@gmail.com"
+var accountPassword = "123456789"
+// accountCity = "Ludhiana"
+// const accountEmail = "rb@gmail.com" ##const cant be redeclare again
+
+// accountId = 1
+console.log(accountId);
+let variable;
+
+/*
+prefer not to use var because of issue in block scope and function scope
+
+*/
+console.table([ accountEmail,accountId,accountPassword, variable ])// for printing all variables
