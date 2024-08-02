f15d33fc729794b8a6a9c8f492322b3a69bc35da
RichaLogiciel <richa@logiciel.io>
2024-08-02 17:52:23 +0530

    changes done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,3 +24,39 @@ let stringNumber = String(someNumber)
 
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+
+// -------------operations-----------
+
+let value = 33
+let negValue = -value;
+console.log(negValue);
+
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);
+
+
+let str1 = "hish1";
+let str2 = "hish2";
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(2 + "3");
+console.log("1" + 2 + 2);
+console.log(1  + 2 + "2");
+
+console.log( (3 + 2) * 4 - 2 ); //use () to separate code 
+console.log(true); //output true
+console.log(+true);// output 1 dont prefer
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2;
+
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,11 @@
+console.log(2 > 1);
+// >=,<,<=,==,!-=
+// 2 cheezo ko compare krne se pehle y check zrur kre ki dono ki datatype same history
+console.log("2" > 1);
+
+console.log(null > O);
+console.log(null = 0);
+console.log(null < 0);
+
+
+
